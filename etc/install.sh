5d32f7b6e9351677f179d05b84751eccef1e8f62
ea7kir <mikenaylorspain@icloud.com>
2024-07-25 10:12:19 +0200

    updated

--- etc/install.sh
+++ etc/install.sh
@@ -3,9 +3,11 @@
 # Install Q100 PA Server on Raspberry Pi 4
 # Orignal design by Michael, EA7KIR
 
-# updated to GO 1.25 on July 6 2024
-
 GOVERSION=1.22.5
+
+#
+# TODO: update for Bookworm
+#
 
 whoami | grep -q pi
 if [ $? != 0 ]; then
@@ -28,68 +30,93 @@ while true; do
     esac
 done
 
+echo "
 ###################################################
+Update Pi OS
+###################################################
+"
 
-echo Update Pi OS
 sudo apt update
 sudo apt -y full-upgrade
 sudo apt -y autoremove
 sudo apt clean
 
+echo "
 ###################################################
+Making changes to config.txt
+###################################################
+"
 
 # echo Running rfkill # not sure if this dupicates config.txt
 # rfkill block 0
 # rfkill block 1
 
-###################################################
-
-echo Making changes to config.txt
-
 sudo sh -c "echo '\n# EA7KIR Additions' >> /boot/config.txt"
 
-echo Disable Wifi
 sudo sh -c "echo 'dtoverlay=disable-wifi' >> /boot/config.txt"
 
-echo Disable Bluetooth
 sudo sh -c "echo 'dtoverlay=disable-bt' >> /boot/config.txt"
 
-echo Enable 1-Wire
-sudo sh -c "echo 'dtoverlay=w1-gpio' >> /boot/config.txt"
-
-echo Enable I2C
-# sudo sh -c "echo 'dtparam=i2c_arm=on' >> /boot/config.txt"
-sudo sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/g' /boot/config.txt
-
+echo "
 ###################################################
+Ebable 1-Wire and I2C
+###################################################
+"
 
-echo Making changes to .profile
+# sudo sh -c "echo 'dtoverlay=w1-gpio' >> /boot/config.txt"
+sudo raspi-config nonint do_onewire 0
+
+## sudo sh -c "echo 'dtparam=i2c_arm=on' >> /boot/config.txt"
+#sudo sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/g' /boot/config.txt
+sudo raspi-config nonint do_i2c 0
+
+echo "
+###################################################
+Making changes to .profile
+###################################################
+"
 
 sudo sh -c "echo '\n# EA7KIR Additions' >> /home/pi/.profile"
 
-echo Adding go path to .profile
 echo -e 'export PATH=$PATH:/usr/local/go/bin' >> /home/pi/.profile
 
+echo "
 ###################################################
+Installing I2C Tools
+###################################################
+"
 
-echo Installing i2c-tools
 sudo apt -y install i2c-tools
 
-echo Installing Go $GOVERSION
+echo "
+###################################################
+Installing Go $GOVERSION
+###################################################
+"
+
 GOFILE=go$GOVERSION.linux-arm64.tar.gz
 cd /usr/local
 sudo wget https://go.dev/dl/$GOFILE
 sudo tar -C /usr/local -xzf $GOFILE
 cd
 
-echo Copying q100paserver.service
+echo "
+###################################################
+Copying q100receiver.service
+###################################################
+"
+
 cd /home/pi/Q100/q100paserver/etc
 sudo cp q100paserver.service /etc/systemd/system/
 sudo chmod 644 /etc/systemd/system/q100paserver.service
 sudo systemctl daemon-reload
 cd
 
+echo "
 ###################################################
+Prevent this script form being executed again
+###################################################
+"
 
 chmod -x /home/pi/Q100/etc/install.sh # to prevent it from being run a second time
 
